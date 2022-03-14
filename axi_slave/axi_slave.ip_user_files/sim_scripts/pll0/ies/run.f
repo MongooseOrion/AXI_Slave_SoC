3cbf2b55963fe89984602b1adc10d42bc70e3b7f
MongooseOrion <64147607+MongooseOrion@users.noreply.github.com>
2022-03-14 12:18:39 +0800

    v0.2

---
+++ axi_slave/axi_slave.ip_user_files/sim_scripts/pll0/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "D:/Xilinx/Vivado/2018.3/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "D:/Xilinx/Vivado/2018.3/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../../pll0/pll0_clk_wiz.v" \
+  "../../../../../pll0/pll0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
